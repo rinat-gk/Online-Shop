a1ad633c803f1c4e84b433abd113d24ccda60405
rinat-gk <53332783+rinat-gk@users.noreply.github.com>
2020-06-17 00:42:59 +0400

    Add project files.

---
+++ Shop/Data/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Models
+{
+    public class Car
+    {
+        public int id { set; get; }
+
+        public string name { set; get; }
+
+        public string shortDescr { set; get; }
+
+        public string longDescr { set; get; }
+
+        public string img { set; get; }
+
+        public ushort price { set; get; }
+
+        public bool isFavorite { set; get; }
+
+        public int available { set; get; }
+
+        public int categoryID { set; get; }
+
+        public virtual Category Category { set; get; }
+
+
+    }
+}
